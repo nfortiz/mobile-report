d804314200725fa42f1d08acd04dc73bb3794e2f
Fernando Ortiz Suarez <nf.ortiz@uniandes.edu.co>
2020-05-13 14:40:28 -0500

    Added new Env Variables

--- src/server/config/index.js
+++ src/server/config/index.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
-export const config = {
-  dev: process.env.ENV !== 'production',
+const config = {
+  dev: process.env.NODE_ENV !== 'production',
   port: process.env.PORT || 3000,
   githubOauthToken: process.env.GITHUB_OAUTH_TOKEN
 }
+
+module.exports = { config }
